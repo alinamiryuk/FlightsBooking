e201c0f8d031ea79b8112aed9b53eb9507515ba9
alinamiryuk <alinamiryuk@mail.ru>
2021-01-07 14:43:02 +0300

    font

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './redux/store'
 import { BrowserRouter } from 'react-router-dom'
+import 'typeface-roboto'
 
 ReactDOM.render(
   <Provider store={store}>
